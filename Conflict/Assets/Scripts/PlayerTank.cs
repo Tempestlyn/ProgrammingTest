0c1b47f886e0c8afc85ecbef8334edbaa3817ef0
Tempestlyn <31550496+Tempestlyn@users.noreply.github.com>
2021-06-15 19:09:44 -0400

    Assets organized. Map created.

---
+++ Conflict/Assets/Scripts/PlayerTank.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTank : MonoBehaviour
+{
+    public float Speed;
+    public float TurnSpeed;
+    public List<Weapon> Weapons;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
