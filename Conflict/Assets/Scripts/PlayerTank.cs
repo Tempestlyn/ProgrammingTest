182eb8314d0194cfd209652ede67b481a21bade3
Tempestlyn <31550496+Tempestlyn@users.noreply.github.com>
2021-06-16 15:49:22 -0400

    Tank movement added.
    Acceleration and deceleration.

--- Conflict/Assets/Scripts/PlayerTank.cs
+++ Conflict/Assets/Scripts/PlayerTank.cs
@@ -1,20 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerTank : MonoBehaviour
 {
-    public float Speed;
+    public float CurrentSpeed;
+    public float MaxForwardSpeed;
+    public float MaxBackwardSpeed;
+    public float Acceleration;
+    public float Deceleration;
+    
     public float TurnSpeed;
     public List<Weapon> Weapons;
-    
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
+
         
+        if ((Input.GetKey(KeyCode.W)) && (CurrentSpeed < MaxForwardSpeed))
+        {
+            CurrentSpeed = CurrentSpeed + (Acceleration * Time.deltaTime);
+        }
+        else if ((Input.GetKey(KeyCode.S)) && (CurrentSpeed > -MaxBackwardSpeed))
+        {
+            CurrentSpeed = CurrentSpeed - (Acceleration * Time.deltaTime);
+        }
+        else
+        {
+            if (Math.Abs(CurrentSpeed) > Deceleration * Time.deltaTime)
+                if (CurrentSpeed > 0)
+                {
+                    CurrentSpeed = CurrentSpeed - (Deceleration * Time.deltaTime);
+
+                }
+                else
+                {
+                    CurrentSpeed = CurrentSpeed + (Deceleration * Time.deltaTime);
+                }
+            else
+            {
+                CurrentSpeed = 0;
+            }
+        }
+
+        transform.position -= transform.up * (CurrentSpeed * Time.deltaTime);
+
+        
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.forward * TurnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(-Vector3.forward * TurnSpeed * Time.deltaTime);
+
     }
 }
